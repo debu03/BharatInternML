870a1d2e29d0ab1969fff05d02d1399c6519380d
Debanjana Debjyoti Chanda <debu@Debanjanas-MacBook-Air.local>
2023-11-13 21:00:27 +0530

    changes

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import joblib as joblib
 import os
 
-model = joblib.load('saved_model1.pkl')
+model = joblib.load('iris_model.pkl')
 scaler = joblib.load('scaler.save')
 
 app = Flask(__name__)
